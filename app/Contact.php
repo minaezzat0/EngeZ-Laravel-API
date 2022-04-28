e4caa2b56e76b7b806c92503242262a1fed16ffc
Mina Isaac <43214610+mina-isaac-99@users.noreply.github.com>
2022-04-28 21:26:48 +0200

    adding email

--- app/Contact.php
+++ app/Contact.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     //
-    protected $fillable=['user_id','subject','message'];
+    protected $fillable=['email','subject','message'];
 
     public function user(){
         return $this->belongsTo('App\User');
